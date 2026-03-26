3f10279e321d7af75a45e28aa1e408c18310a9ec
alexer42 <alexandre.dziubate@escola.pr.gov.br>
2026-03-26 15:05:21 +0000

    versao 1

---
+++ script.js
@@ -0,0 +1,21 @@
+//booleano
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+//falso e verdadeiro
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+//undefined
+
+let minhaVar;
+let varnull = null;
+
+let numero = 3;
+let texto = "alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varnull)
